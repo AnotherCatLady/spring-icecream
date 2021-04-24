63f8d1fb09570c74ebbcecd3b89535888c5348fe
Kitty <weiss-jennifer@outlook.de>
2021-04-25 01:43:06 +0200

    added typedoc

--- angular/src/app/service/icecream.service.ts
+++ angular/src/app/service/icecream.service.ts
@@ -3,21 +3,32 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Icecream} from "../model/icecream";
 
+/**
+ * Service to fetch and save icecreams.
+ */
 @Injectable({providedIn: 'root'})
 export class IcecreamService {
 
   private baseUrl = 'http://localhost:8080/icecream'
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
-  constructor(   private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
+  /**
+   * Get all icecreams from backend.
+   */
   getIcecreams(): Observable<Icecream[]> {
     return this.http.get<Icecream[]>(this.baseUrl).pipe();
   }
 
+  /**
+   * Save new icecream to backend.
+   * @param icecream icecream to save.
+   */
   saveIcecream(icecream: Icecream): Observable<any> {
     return this.http.put(this.baseUrl, icecream, this.httpOptions).pipe();
   }
